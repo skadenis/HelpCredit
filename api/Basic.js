52dd3960a0d60d1f40961f2e46dff955784c66b4
skadenis@mail.ru <Tutanu2211>
2022-06-13 14:26:01 +0300

    BankCredit.by

--- api/Basic.js
+++ api/Basic.js
@@ -3,7 +3,7 @@ import BaseApi from "./BaseApi";
 class Basic extends BaseApi {
 
   sendLead (data) {
-    data.token = 'fejkbcksnacpewmcnjknbhjfdbvncsd';
+    data.token = '1279t7ehduohwe1udh10d0yge9792';
     return this.axios.post('/add-lead', data);
   }
 
