1a0e199c39fb4cf7720c3868c8b6c1926f241db8
skadenis@mail.ru <Tutanu2211>
2022-06-23 21:42:02 +0300

    Revert "BankCredit.by"
    This reverts commit c649a1ec5862a6fb29d1a56b594ab131afd4f817.

--- api/Basic.js
+++ api/Basic.js
@@ -2,7 +2,7 @@ import BaseApi from "./BaseApi";
 
 class Basic extends BaseApi {
   sendLead(data) {
-    data.token = "gyiucbwyouceiuwhednewiuyd72btydghnulmuhox";
+    data.token = "2xwr9egn348f9237fz234fmz023f70";
     return this.axios.post("/add-lead", data);
   }
 }
--- src/components/banki/bankhelp.vue
+++ src/components/banki/bankhelp.vue
@@ -1,30 +1,131 @@
 <template>
-  <div class="">
-    <h1>Про</h1>
-    <button @click="$emit('someEvent')">
-      ПРОЙТИ ТЕСТ
-    </button>
+  <div>
+    <div class="wrapper">
+      <div class="wrapper-block">
+        <div>
+          <span class="header"
+            >Оформи любой кредит<br />
+            в Минске по ставке <br /><span
+              >от <span class="percent">12,9%</span></span
+            ></span
+          >
+        </div>
+        <div class="button-block">
+          <button @click="$emit('someEvent')" class="button">
+            Подобрать кредит
+          </button>
+          <div class="statistics">
+            <p>
+              Осталось всего <span>{{ remainder }}</span>
+            </p>
+            <p>
+              Сегодня одобрено <span>{{ quantity }}</span>
+            </p>
+          </div>
+          <p class="date">
+            Пройдите тест и получите самое <br />
+            выгодное предложение на {{ today }}
+          </p>
+        </div>
+      </div>
+      <partnerslogo />
+    </div>
+    <disclamer siteName="BankHelp.by" />
   </div>
 </template>
 
 <script>
-export default {
-  name: 'bankHelp',
-  methods: {
+import disclamer from "@/components/quiz/disclamer.vue";
+import partnerslogo from "@/components/banki/partnerslogo.vue";
 
-  }
-}
+export default {
+  name: "bank-help",
+  data() {
+    return {
+      remainder: "9 кредитов",
+      quantity: 37,
+      today: undefined,
+    };
+  },
+  mounted() {
+    this.getDate();
+  },
+  methods: {
+    getDate() {
+      this.today = new Date();
+      let dd = String(this.today.getDate()).padStart(2, "0");
+      let mm = String(this.today.getMonth() + 1).padStart(2, "0");
+      this.today = dd + "." + mm;
+    },
+  },
+  components: {
+    disclamer,
+    partnerslogo,
+  },
+};
 </script>
 
-<style>
-button{
-  position: fixed;
-  bottom: 15px;
-  left: 15px;
-  right: 15px;
-  width: calc(100% - 30px);
-  padding: 25px;
-  font-size: 25px;
+<style scoped>
+@import url("https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700;800;900&display=swap");
+* {
+  font-family: "Montserrat", sans-serif;
+}
+
+.wrapper {
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  min-height: 100vh;
+
+  padding: 75px 100px 25px 100px;
+
+  background-color: rgb(80, 120, 180);
+  background-image: linear-gradient(180deg, rgba(0, 0, 0, 0.5), transparent),
+    url(../../assets/creditcentr_bg.jpg);
+  background-size: cover;
+  background-position: center;
+}
+
+.wrapper-block {
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  /* margin-bottom: 20px; */
+}
+
+.header {
+  display: block;
+
+  font-size: 46px;
+  font-weight: 900;
+  color: rgb(255, 255, 255);
+  line-height: 55.67px;
+  margin-bottom: 50px;
+}
+
+.header span {
+  font-size: 76.47px;
+  line-height: 93.2px;
+}
+
+.header .percent {
+  font-size: 109.72px;
+  line-height: 133.75px;
+  color: rgba(255, 220, 98, 1);
+  text-shadow: 0px 0px 24px rgba(255, 220, 98, 0.25);
+}
+
+.button-block {
+  display: grid;
+  grid-template-areas:
+    "button statistics"
+    "date date";
+  grid-template-rows: 81px 1fr;
+  grid-template-columns: 309px 1fr;
+  column-gap: 30px;
+  row-gap: 20px;
 }
 
 .button {
